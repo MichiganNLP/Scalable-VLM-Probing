19aefd59244ecec5633744d95f1d4f064b554351
Santiago Castro <santi.1410@hotmail.com>
2023-03-06 23:44:42 -0500

    Update the word freq calculation to be more portable

---
+++ compute_word_frequencies.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import itertools
+import json
+import string
+from collections import Counter
+from typing import Iterable
+
+from datasets import load_dataset
+from tqdm.auto import tqdm
+
+
+def load_laion_texts() -> Iterable[str]:
+    for instance in load_dataset("laion/laion400m", split="train", streaming=True):
+        if text := instance["TEXT"]:
+            yield text.lower().translate(str.maketrans("", "", string.punctuation))
+
+
+def main() -> None:
+    max_count = 10_000_000  # This is a reasonable number, and it's also roughly what LAION's 1st parquet file has.
+    word_counts = Counter(word
+                          for text in tqdm(itertools.islice(load_laion_texts(), max_count), total=max_count)
+                          for word in text.split())
+    with open("data/words_counter_LAION.json", "w") as f:
+        json.dump(word_counts, f)
+
+
+if __name__ == "__main__":
+    main()
