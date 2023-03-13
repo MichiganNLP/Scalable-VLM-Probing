71058702a9cff4595a645fa5d4675d5afc79f228
Santiago Castro <sacastro@umich.edu>
2023-03-12 21:47:23 -0400

    Update the Great Lakes script

--- big_run.sh
+++ big_run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 #SBATCH --job-name=clip-svo-probes-feature-importance
-#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-type=BEGIN,FAIL,END
 #SBATCH --cpus-per-task=36
 #SBATCH --mem=180G
-#SBATCH --time=72:00:00
+#SBATCH --time=24:00:00
 #SBATCH --account=mihalcea98
 #SBATCH --partition=standard
 
 module load python/3.10.4
 source venv/bin/activate
-python main.py
+#python main.py --dependent-variable-name pos_clip_score --no-neg-features --vif
+python main.py --dependent-variable-name neg_clip_score --vif
