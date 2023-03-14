935f2a5b85790cbc4ba9555bcb1798a68a815868
Santiago Castro <sacastro@umich.edu>
2023-03-14 11:57:04 -0400

    Update the wall time of the GL script

--- big_run.sh
+++ big_run.sh
@@ -4,11 +4,11 @@
 #SBATCH --mail-type=BEGIN,FAIL,END
 #SBATCH --cpus-per-task=36
 #SBATCH --mem=180G
-#SBATCH --time=16:00:00
+#SBATCH --time=24:00:00
 #SBATCH --account=mihalcea98
 #SBATCH --partition=standard
 
 module load python/3.10.4
 source venv/bin/activate
-#python main.py --dependent-variable-name pos_clip_score --no-neg-features --do-vif
-python main.py --dependent-variable-name neg_clip_score --do-vif
+python main.py --dependent-variable-name pos_clip_score --no-neg-features --do-vif
+#python main.py --dependent-variable-name neg_clip_score --do-vif
