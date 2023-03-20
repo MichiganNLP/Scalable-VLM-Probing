a3bd5050dddb13972ed015c86873465712420ede
Santiago Castro <sacastro@umich.edu>
2023-03-19 21:48:01 -0400

    WIP: compute CLIP scores for another dataset

--- compute_clip_scores.py
+++ compute_clip_scores.py
@@ -5,9 +5,9 @@ import random
 from typing import Any, MutableMapping
 
 import numpy as np
-import requests
 import torch
 from PIL import Image
+from cached_path import cached_path
 from datasets import load_dataset
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
@@ -23,7 +23,7 @@ def parse_args() -> argparse.Namespace:
 
     parser.add_argument("--seed", type=int, default=42)
 
-    parser.add_argument("--device", default="cuda" if torch.cuda.is_available() else "cpu")
+    parser.add_argument("--device", type=torch.device, default="cuda" if torch.cuda.is_available() else "cpu")
 
     parser.add_argument("--dataset", default="red_caps",
                         help="See options at https://huggingface.co/datasets?"
@@ -36,20 +36,17 @@ def parse_args() -> argparse.Namespace:
                         help="See options at https://huggingface.co/models?pipeline_tag=zero-shot-image-classification")
 
     parser.add_argument("--output-path", default="output.pt")
-    args = parser.parse_args()
 
-    args.device = torch.device(args.device)
-
-    return args
+    return parser.parse_args()
 
 
 def fetch_image(instance: Instance) -> Instance:
-    instance["image"] = Image.open(requests.get(instance["image_url"], stream=True).raw)
-    return instance
+    # return {"image": Image.open(requests.get(instance["image_url"], stream=True).raw)}
+    return {"image": Image.open(cached_path(instance["image_url"]))}
 
 
 def preprocess_data(processor: ProcessorMixin, instance: Instance) -> Instance:
-    return processor(text=instance["caption"], images=instance["image"], return_tensors="pt")  # noqa
+    return processor(text=instance["caption"], images=instance["image"])  # noqa
 
 
 def main() -> None:
@@ -67,22 +64,22 @@ def main() -> None:
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
 
     dataset = load_dataset(args.dataset, split="train", streaming=True)
-    dataset = dataset.filter(input_columns=["image_url", "caption"])
-    dataset = dataset.map(fetch_image)
+    dataset = dataset.select_columns(["image_url", "caption"])
+    dataset = dataset.map(fetch_image, remove_columns=["image_url"])
 
     processor = AutoProcessor.from_pretrained(args.model_name_or_path)
-    model = AutoModel.from_pretrained(args.model_name_or_path).to(args.device).eval()
+    dataset = dataset.map(lambda instance: preprocess_data(processor, instance), remove_columns=["image"])
 
-    dataset = dataset.map(lambda instance: preprocess_data(processor, instance))
-    dataset = dataset.with_format("torch")
     data_loader = DataLoader(dataset, batch_size=args.batch_size, num_workers=args.num_workers,
-                             pin_memory=args.device.type == "cuda")
+                             pin_memory=args.device.type != "cpu")
+
+    model = AutoModel.from_pretrained(args.model_name_or_path).to(args.device).eval()
 
     scores = []
 
     with torch.inference_mode():
         for batch in tqdm(data_loader):
-            batch = {k: v.to(args.device) for k, v in batch.items()}
+            batch = {k: v.to(args.device) if isinstance(v, torch.Tensor) else v for k, v in batch.items()}
             output = model(**batch)
             scores.append(output.logits.cpu())
 
